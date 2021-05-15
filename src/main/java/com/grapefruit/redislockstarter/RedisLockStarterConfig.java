6a68a6a211c05d7f887f572a8b5268665dfad713
zzh-1992 <ezzh1992@icloud.com>
2021-05-15 13:54:06 +0800

    添加jedis属性配置类

--- src/main/java/com/grapefruit/redislockstarter/RedisLockStarterConfig.java
+++ src/main/java/com/grapefruit/redislockstarter/RedisLockStarterConfig.java
@@ -1,10 +1,13 @@
 package com.grapefruit.redislockstarter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.Jedis;
+
+import java.util.Optional;
 
 /**
  * @author Grapefruit
@@ -16,12 +19,14 @@ import redis.clients.jedis.Jedis;
 @Configuration()
 public class RedisLockStarterConfig {
 
+    @Autowired
+    JedisProperties properties = new JedisProperties();
     @Bean
     public Jedis jedis(){
-        String host = "127.0.0.1";
-        int port = 6379;
+        String host = Optional.ofNullable(properties.getIp()).orElse("127.0.0.1");
+        int port = Optional.of(properties.getPort()).orElse(6369);
         Jedis jedis = new Jedis(host, port);
-        jedis.auth("123456");
+        jedis.auth(Optional.ofNullable(properties.getPassword()).orElse("6369"));
         return jedis;
     }
 }
